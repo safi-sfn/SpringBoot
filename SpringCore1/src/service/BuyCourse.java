7030939e3f0c624f63faf57560fc66e28aafa3d5
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-03 16:58:10 +0530

    refactor(di): decouple BuyCourse from concrete implementations
    - Introduced DI via ICourse interface to avoid direct dependencies
    - Implemented setter injection for flexible course assignment
    - Delegated object creation responsibility to caller (IoC principle)
    - Encapsulated purchase logic in buyTheCourse() with interface
    delegation

--- SpringCore1/src/service/BuyCourse.java
+++ SpringCore1/src/service/BuyCourse.java
@@ -1,9 +1,30 @@
 package service;
 
 public class BuyCourse {
+	
+	
+	/*
+	 * i want to use 
+	 *   method of JavaCourse class
+	 *   method of SpringBootCourse class
+	 *   method of DevOpsCourse class
+	 *   --but directly i don't want to create an object of javacourse,springbootcourse,devopscourse
+	 *   i will handover the control of object creation to another class called Inversion Control
+	 *   in this program our main method is inversion of Control
+	 */
+
+	private ICourse course;
+	
+	public void setCourse(ICourse course) {
+		this.course = course;
+	}
 
 	public boolean buyTheCourse(double amount) {
-		return true;
+		
+		
+		return  course.getTheCourse(amount);
+		
+
 	}
 
 }
