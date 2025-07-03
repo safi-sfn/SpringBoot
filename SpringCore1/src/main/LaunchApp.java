766e2bfbef00521bd4110b3cc5fce1e7d4839959
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-03 16:56:36 +0530

    feat(DI): demonstrate basic (di) dependency injection implementation
    - Created target class BuyCourse with course dependency
    - Implemented setter-based dependency injection
    - Showcased multiple course implementations:
      * JavaCourse
      * DevOpsCourse
      * SpringBootCourse

--- SpringCore1/src/main/LaunchApp.java
+++ SpringCore1/src/main/LaunchApp.java
@@ -1,9 +1,31 @@
 package main;
 
+import service.BuyCourse;
+import service.DevOpsCourse;
+import service.JavaCourse;
+import service.SpringBootCourse;
+
 public class LaunchApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BuyCourse buyCourse = new BuyCourse();  //target class/object
+		
+//		JavaCourse java = new JavaCourse();  --> Dependent object
+//		buyCourse.setCourse(java);
+		
+//		injecting dependent object into target object is called -> Dependency Injection
+		
+		// Setter injection
+		buyCourse.setCourse(new JavaCourse());
+//		buyCourse.setCourse(new DevOpsCourse());
+//		buyCourse.setCourse(new SpringBootCourse());
+		
+		boolean status = buyCourse.buyTheCourse(4500);
+		if(status) 
+			System.out.println("Enrolled the courses");
+		else
+			System.out.println("failed to enroll");
 
 	}
 
