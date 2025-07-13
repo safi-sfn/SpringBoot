279fccd6049529846ff4c4acc387e94e68a19228
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-13 14:31:22 +0530

    constructor created

--- SpringCoreXML1/src/main/java/service/BuyCourse.java
+++ SpringCoreXML1/src/main/java/service/BuyCourse.java
@@ -2,19 +2,16 @@ package service;
 
 public class BuyCourse {
 	
-	
-	/*
-	 * i want to use 
-	 *   method of JavaCourse class
-	 *   method of SpringBootCourse class
-	 *   method of DevOpsCourse class
-	 *   --but directly i don't want to create an object of javacourse,springbootcourse,devopscourse
-	 *   i will handover the control of object creation to another class called Inversion Control
-	 *   in this program our main method is inversion of Control
-	 */
 
 	private ICourse course;
 	
+	
+	
+	public BuyCourse() {
+		
+		System.out.println("Course bean created");
+	}
+
 	public void setCourse(ICourse course) {
 		this.course = course;
 	}
