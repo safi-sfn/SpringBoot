cff7b2e61353ce3cb156a784df866165631557ef
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-13 14:31:53 +0530

    constructor created for bean debug

--- SpringCoreXML1/src/main/java/service/DevOpsCourse.java
+++ SpringCoreXML1/src/main/java/service/DevOpsCourse.java
@@ -4,8 +4,12 @@ public class DevOpsCourse implements ICourse {
 
 	@Override
 	public boolean getTheCourse(double amount) {
+		
 		System.out.println("DevOps course purchased and amount paid is "+amount);
 		return true;
 	}
+
+	public DevOpsCourse() {
+		System.out.println("DevOps bean created");	}
 
 }
